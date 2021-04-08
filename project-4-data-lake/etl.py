13832a427b0e1b92f5efa17a7c5adeb8b5069c43
ernani.murtinho <ernani.murtinho@zooxsmart.com>
2021-04-07 22:19:42 -0300

    Alteracoes para execucao da primeira carga teste

--- project-4-data-lake/etl.py
+++ project-4-data-lake/etl.py
@@ -77,18 +77,26 @@ def process_log_data(spark, input_data, output_data="s3n://awsds/"):
 
     # extract columns to create time table
     time_table = df.selectExpr(
-        "start_time, hour(start_date) as hour, day(start_date) as day, weekofyear(start_date) as week, month(start_date) as month, year(start_date) as year, weekday")
+        "start_time, hour(start_date) as hour, day(start_date) as day, "
+        "weekofyear(start_date) as week, month(start_date) as month, "
+        "year(start_date) as year, weekday")
 
     # write time table to parquet files partitioned by year and month
     time_table.write.partitionBy("year", "artist_id").mode('append').parquet(timetable_out)
 
     # read in song data to use for songplays table
+    artists_out = "{out_data}artists".format(out_data="s3n://awsds/")
+
+    artists_df = spark.read.json(artists_out)
     song_df = spark.read.json(song_data)
 
+    songs_join = df.join(song_df, df.song == song_df.title)
+    songplays = artists_df.join(songs_join, artists_df.name == song_df.artist, how='inner')
+
     # extract columns from joined song and log datasets to create songplays table
-    songplays_table = df.select(
-        "songplay_id", "start_time", "user_id", "level",
-        "song_id", "artist_id", "session_id", "location", "user_agent"
+    songplays_table = songplays.selectExpr(
+        "songplay_id, ts as start_time, userId AS user_id, level,"
+        "song_id, artist_id, sessionId AS session_id, location, userAgent AS user_agent"
     )
 
     # write songplays table to parquet files partitioned by year and month
